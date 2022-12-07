7e9beda9065f362e12c1c9c98e4deba8c31acbc4
Mohammed <68031021+MohammedJava@users.noreply.github.com>
2022-12-07 00:57:08 -0500

    CSS changes

--- src/Volunteering/Volunteering.jsx
+++ src/Volunteering/Volunteering.jsx
@@ -1,9 +1,7 @@
-
 import React from "react";
 import "./Volunteering.css";
 import Tilt from "react-parallax-tilt";
 /* eslint-disable react/no-unescaped-entities */
-
 
 const Volunteering = () => (
   <div className="Volunteering" id="Volunteering">
@@ -60,64 +58,63 @@ const Volunteering = () => (
           </div>
           <div id="item-1">
             <p>
-            The results of our survey demonstrated that an overwhelming number
-            of users who require special food, such as, halal, gluten free and
-            lactose free products, find it difficult to find places which
-            provide these services.
+              The results of our survey demonstrated that an overwhelming number
+              of users who require special food, such as, halal, gluten free and
+              lactose free products, find it difficult to find places which
+              provide these services.
             </p>
             <br />
-            <br /> 
+            <br />
             <p>
-            Almost 60% of the participants reponseded having dietary
-            restrictions
+              Almost 60% of the participants reponseded having dietary
+              restrictions
             </p>
             <br />
-            <br /> 
-            <p>Almost a third of all the participants chose restaurants
-            reviews as the main factor when looking for a resturant seconded by
-            the price of the menu by one fourth of the participants. The other
-            main factors to choose a restaurant were the culinary restrictions,
-            the distance and the nationality of the food.
+            <br />
+            <p>
+              Almost a third of all the participants chose restaurants reviews
+              as the main factor when looking for a resturant seconded by the
+              price of the menu by one fourth of the participants. The other
+              main factors to choose a restaurant were the culinary
+              restrictions, the distance and the nationality of the food.
             </p>
           </div>
           <div id="item-6">
             <p>
-            Secondly, users find it important to support their local restaurants
-            rather than big chain franchises. On the other hand, when it comes
-            to delivery, the users have no preference to whether or not it is
-            delivered directly from the restaurant or by a 3rd party, like
-            UberEats for example. They also believe that eating out at
-            restaurants is much more important than ordering out, yet these
-            results are very close to each other as each option has almost equal
-            participation.
+              Secondly, users find it important to support their local
+              restaurants rather than big chain franchises. On the other hand,
+              when it comes to delivery, the users have no preference to whether
+              or not it is delivered directly from the restaurant or by a 3rd
+              party, like UberEats for example. They also believe that eating
+              out at restaurants is much more important than ordering out, yet
+              these results are very close to each other as each option has
+              almost equal participation.
             </p>
             <br />
             <br />
             <br />
             <p>
-            Thirdly, when it comes to the crowdedness of the user's local
-            restaurants, the results show that the restaurants are not full of
-            people, rather they are crowded the same day to day. This is an
-            important factor because the crowdedness of a restaurant will affect
-            the user's decision if they would want to eat at said place.
+              Thirdly, when it comes to the crowdedness of the user's local
+              restaurants, the results show that the restaurants are not full of
+              people, rather they are crowded the same day to day. This is an
+              important factor because the crowdedness of a restaurant will
+              affect the user's decision if they would want to eat at said
+              place.
             </p>
             <br />
             <br />
             <br />
             <p>
-            Lastly, a large part of the decision making process for choices in a
-            restaurant are the reviews other people give. This will help the
-            user make a personal decision on whether or not they want to eat
-            there given the past experiences of other people.
+              Lastly, a large part of the decision making process for choices in
+              a restaurant are the reviews other people give. This will help the
+              user make a personal decision on whether or not they want to eat
+              there given the past experiences of other people.
             </p>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 );
 
-
 export default Volunteering;
-
